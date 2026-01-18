de0ccf18b16ea5a9cd7cf0a39a534c990ad3a32d
usman-malik2636 <ultimategamerdevil@gmail.com>
2026-01-18 14:24:20 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+const CACHE_NAME='dw-pwa-v1';
+const ASSETS=['./','./index.html','./focus.html','./manifest.json'];
+self.addEventListener('install',e=>{
+ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('fetch',e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
